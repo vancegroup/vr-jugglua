b7fa757888cb80883f8fb7e98cc324b3c96cf409
Ryan Pavlik <rpavlik@iastate.edu>
2010-05-27 17:25:47 -0500

    Improve osgnav

--- examples/osgnav.lua
+++ examples/osgnav.lua
@@ -4,12 +4,15 @@ print("Loading lua file osgnav.lua")
 
 -- sets the variable "scene"
 --loadfile("sceneconfig.lua")
+node = osgLua.loadObjectFile('/home/rpavlik/.gvfs/sftp for rpavlik on keymaker.vrac.iastate.edu/home/users/mperkins/export/bottle.obj')
 scene = osg.PositionAttitudeTransform()
+scene:addChild(node)
+--[[osg.PositionAttitudeTransform()
 geode = osg.Geode()
 shape = osg.ShapeDrawable()
-shape:setShape( osg.Sphere(osg.Vec3(0,0,0),15) )
+shape:setShape( osg.Sphere(osg.Vec3(20,20,20),5) )
 geode:addDrawable(shape)
-scene:addChild(geode)
+scene:addChild(geode)]]--
 
 function showInfo(instance)
 	local c = osgLua.getTypeInfo(instance)
@@ -26,53 +29,52 @@ function showInfo(instance)
 	end
 end
 
+function vecToString(vec)
+	return "(" .. tostring(vec:x()) .. ", " .. tostring(vec:y()) .. ", " .. tostring(vec:z()) .. ")"
+end
+
 
 function createWandNavigator(wand, button, max_vel)
 	return {
 		vel = 0,
 		goal_vel = 0,
 		getPosition = function(self, dt, prev)
-			print("WandNavigator: Checking button")
 			if button:isPressed() == true then
 				-- Set our goal velocity
-				self.goal_vel = self.max_vel
+				self.goal_vel = max_vel
 			else
 				-- Slow down
 				self.goal_vel = 0
 			end
 
-			print("WandNavigator: Updating velocity")
+
 			-- If we aren't at our goal velocity, increase by 1/30th
 			-- or decrease by 1/10th our max velocity, per second
 			if self.vel < self.goal_vel then
-				self.vel = self.vel + (self.goal_vel/30) * dt
+				self.vel = self.vel + (max_vel/30) * dt
 				-- Enforce the max velocity
 				if self.vel > self.goal_vel then self.vel = self.goal_vel end
 
 			elseif self.vel > self.goal_vel then
-				self.vel = self.vel - (self.goal_vel/10) * dt
+				self.vel = self.vel - (max_vel/10) * dt
 				-- Enforce the minimum velocity
 				if self.vel < self.goal_vel then self.vel = self.goal_vel end
 			end
 
+			--print("WandNavigator: Velocity is now " .. self.vel)
+
 			-- Scale the velocity by time and multiply by the direction
 			-- of the wand
-			print("WandNavigator: computing translation magnitude")
 			local mvmt = dt * self.vel
-
-			print("WandNavigator: getting wand forward vector")
-			showInfo(wand:getForwardVector())
 			local fwd = osg.Vec3()
 			fwd:set(wand:getForwardVector())
 			local magnitude = fwd:length()
-			showInfo(fwd)
 
 
 			local xlate = osg.Vec3(
 				prev:x() - (fwd:x() / magnitude) * mvmt,
 				prev:y() - (fwd:y() / magnitude) * mvmt,
 				prev:z() - (fwd:z() / magnitude) * mvmt)
-			print("WandNavigator: vector is " .. tostring(xlate:x()) .. ", " .. tostring(xlate:y()) .. ", " .. tostring(xlate:z()))
 			return xlate
 		end
 	}
@@ -107,6 +109,7 @@ osgnav = {
 	preFrame = function (self)
 		print("In preFrame")
 		local xlate
+		print("Location of the world: " .. vecToString(self.position))
 
 		print("Getting translation from navigator")
 		self.position:set(self.wandnav:getPosition(self.appProxy:getTimeDelta(), self.position))
