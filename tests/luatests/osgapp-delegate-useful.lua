80cda0b715519c4a9496278703934bd4f2de1226
Ryan Pavlik <rpavlik@iastate.edu>
2010-06-03 10:10:59 -0500

    Cleanup messages and test app

--- tests/luatests/osgapp-delegate-useful.lua
+++ tests/luatests/osgapp-delegate-useful.lua
@@ -1,25 +1,23 @@
 print("Loading lua file")
 
-testapp = {
-	counter = 0,
-	max = 10,
+testapp = {counter = 0,
+	max = 10}
 
-	initScene = function (self)
-		print("In initScene")
+
+function testapp:initScene()
+	print("In initScene")
+end
+
+
+function testapp:preFrame(dt)
+	print("In preFrame")
+	self.counter = self.counter + 1
+	print("Counter at " .. tostring(self.counter))
+	if self.counter >= self.max then
+		print("Counter at maximum")
+		vrjKernel.stop()
 	end
-	,
-
-	preFrame = function (self)
-		print("In preFrame")
-		self.counter = self.counter + 1
-		print("Counter at " .. tostring(self.counter))
-		if self.counter >= self.max then
-			print("Counter at maximum")
-			vrjKernel.stop()
-		end
-	end
-}
-
+end
 
 print("App delegate defined, now creating OsgAppProxy")
 testapp.appProxy = vrjApp.OsgAppProxy()
