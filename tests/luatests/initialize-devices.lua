e7194e0b283522598d843571a2c05d98e6bcc1ac
Ryan Pavlik <rpavlik@iastate.edu>
2010-06-23 16:27:51 -0500

    Fix segfault on stopping kernel in tests

--- tests/luatests/initialize-devices.lua
+++ tests/luatests/initialize-devices.lua
@@ -3,9 +3,19 @@ print("In Lua script initialize-devices.lua")
 vrjKernel.loadConfigFile("standalone.jconf")
 vrjKernel.start()
 
+print("Setting up position interface")
 wand = gadget.PositionInterface("VJWand")
 
-
+print("Setting up digital interface")
 button = gadget.DigitalInterface("VJButton0")
 
+print("Setting up analog interface")
+analog = gadget.AnalogInterface("VJAnalogAxis0")
+
+-- Sleep for two seconds to not have a race condition shutting down the kernel
+require("sleep")
+sleep(2.0)
+
+
+print("Stopping kernel...")
 vrjKernel.stop()
---
+++ vrjugglua/lua/sleep.lua
@@ -0,0 +1,15 @@
+-- see http://lua-users.org/wiki/SleepFunction
+function sleep(n)  -- seconds
+  if os.getenv("USER") ~= nil then
+    -- Unix
+    os.execute("sleep " .. tonumber(n))
+  elseif os.getenv("PROGRAMFILES") ~= nil then
+    -- Windows
+    os.execute("ping -n " .. tonumber(n) .. " localhost > NUL")
+  else
+    -- No idea
+    local t0 = os.clock()
+    print("Sleeping for " .. tostring(n) .. " seconds")
+    while os.difftime(os.clock(), t0) <= n and os.clock() > t0 do end
+  end
+end
