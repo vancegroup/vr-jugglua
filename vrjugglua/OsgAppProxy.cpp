5d4a45323f4a371bfb055fbcd244a6d76c5b2955
Ryan Pavlik <rpavlik@iastate.edu>
2010-09-23 16:03:03 -0500

    null pointer fix - still a race condition here.

--- vrjugglua/OsgAppProxy.cpp
+++ vrjugglua/OsgAppProxy.cpp
@@ -153,9 +153,11 @@ void OsgAppProxy::addModelSearchPath(std::string const& path) {
 
 void OsgAppProxy::configSceneView(osgUtil::SceneView* newSceneViewer) {
 	vrj::OsgApp::configSceneView(newSceneViewer);
-	newSceneViewer->getLight()->setAmbient(osg::Vec4(0.5f,0.5f,0.5f,0.5f));
-	newSceneViewer->getLight()->setDiffuse(osg::Vec4(0.0f,0.0f,0.0f,0.0f));
-	newSceneViewer->getLight()->setSpecular(osg::Vec4(0.5f,0.5f,0.5f,0.5f));
+	if (newSceneViewer->getLight()) {
+		newSceneViewer->getLight()->setAmbient(osg::Vec4(0.5f,0.5f,0.5f,0.5f));
+		newSceneViewer->getLight()->setDiffuse(osg::Vec4(0.0f,0.0f,0.0f,0.0f));
+		newSceneViewer->getLight()->setSpecular(osg::Vec4(0.5f,0.5f,0.5f,0.5f));
+	}
 
 	// setup the ambient light the way I want it
 	osg::LightModel* lightmodel = new osg::LightModel;
