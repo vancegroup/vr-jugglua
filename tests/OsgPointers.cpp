6c71ac246847e1ad5b63100fa4da726c689808e6
Ryan Pavlik <rpavlik@iastate.edu>
2010-11-09 17:01:35 -0600

    add a test that won't compile right now

---
+++ tests/OsgPointers.cpp
@@ -0,0 +1,46 @@
+/** @date	2010
+
+	@author
+	Ryan Pavlik ( <rpavlik@iastate.edu> http://academic.cleardefinition.com/ ),
+	Iowa State University
+	Virtual Reality Applications Center and
+	Human-Computer Interaction Graduate Program
+*/
+
+#define BOOST_TEST_MODULE OsgPointers tests
+
+// Internal Includes
+#include <vrjugglua/osgLuaBind.h>
+#include <vrjugglua/LuaScript.h>
+
+// Library/third-party includes
+#include <BoostTestTargetConfig.h>
+#include <osg/Node>
+
+// Standard includes
+#include <iostream>
+
+
+using namespace boost::unit_test;
+using namespace vrjLua;
+using namespace luabind;
+
+void nodeFunc(osg::Node * node) {
+	std::cout << "Got the pointer " << node << std::endl;
+}
+
+struct Fixture {
+	Fixture() {
+		LuaStatePtr ptr(s.getLuaState().lock());
+		module(ptr.get())[
+			def("nodeFunc", &nodeFunc)
+		];
+	}
+	LuaScript s;
+
+};
+
+BOOST_AUTO_TEST_CASE(DefaultConstruction) {
+	Fixture f;
+}
+
