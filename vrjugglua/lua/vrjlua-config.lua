e281f43a7e8545d76f271c85713b243b8f52b91e
Ryan Pavlik <rpavlik@iastate.edu>
2011-01-07 14:48:25 -0600

    use c6 configs

--- vrjugglua/lua/vrjlua-config.lua
+++ vrjugglua/lua/vrjlua-config.lua
@@ -1,2 +1,3 @@
 print("Loading config files into kernel")
-vrjKernel.loadConfigFile("standalone.jconf")
+vrjKernel.loadConfigFile("/home/vr/vjconfig/c6/C6_tracked.jconf")
+vrjKernel.loadConfigFile("/home/users/rpavlik/jconf-juggler22/c6vrjlua-runbuffer.jconf")
