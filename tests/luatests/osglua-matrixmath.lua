2d1e2bab4bdf87d452fb66933fc52545135fc18f
Ryan Pavlik <rpavlik@iastate.edu>
2011-01-13 15:31:28 -0600

    improve tests for matrices

--- tests/luatests/osglua-matrixmath.lua
+++ tests/luatests/osglua-matrixmath.lua
@@ -1,6 +1,11 @@
 
 print("CTEST_FULL_OUTPUT")
 
-print(osg.Matrix())
+print(osg.Matrix.identity())
+
+print(osg.Matrix.identity() == osg.Matrix.identity())
+assert(osg.Matrix.identity() == osg.Matrix.identity())
+
+assert((osg.Matrix.identity() * osg.Matrix.identity())== osg.Matrix.identity())
 
 print("Done!")
