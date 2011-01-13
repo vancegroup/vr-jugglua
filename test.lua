401e285be039d8a96ee06d21348e95fb68f916d2
Ryan Pavlik <rpavlik@iastate.edu>
2011-01-13 09:35:29 -0600

    cleanup, put it in a table

--- test.lua
+++ test.lua
@@ -1,57 +1,51 @@
 local docstrings = setmetatable({}, {__mode = "kv"})
-local function createConcat(decorationFunc)
-	local mt = {__concat = decorationFunc}
-	return function(...)
-		return setmetatable({...}, mt)
-	end
-end
-
 local mt = {}
+local helpExtensions = {}
 local help = setmetatable({}, mt)
 
 function mt:__call(obj,...)
 	print(arg.n)
-	if docstrings[obj] then
-		print("Help:", table.concat(docstrings[obj], ","))
+	local helpContent = help.lookup(obj)
+	
+	if helpContent then
+		print("Help:", helpContent)
 	else
-		print("No help available!")
+		print("No help available!")	
 	end
 end
 
-decorator = createConcat(function(a,f)
-	    return function(...)
-	      print("decorator", table.concat(a, ","), ...)
-	      return f(...)
-	    end
-	  end
-)
---[[
-docstring = createConcat(
-	function(a,f)
-		return function(...)
-			if docstrings[f] then
-				table.insert(docstrings[f], a)
-			else
-				docstrings[f] = {a}
-			end
-			return f
+function help.lookup(obj)
+	if docstrings[obj] then
+		 return table.concat(docstrings[obj], ",")
+	end
+	for _, v in ipairs(helpExtensions) do
+		local helpContent = v(obj)
+		if helpContent then 
+			return helpContent
 		end
 	end
-)]]
+end
 
 function help.docstring(docs, ...)
 	print("arg to docstring: " .. docs)
 	print("Extra args to docstring: " .. arg.n)
 	local mt = {}
+	-- handle the .. operator for inline documentation
 	function mt.__concat(a, f)
 		docstrings[f] = {docs, unpack(arg)}
 		return f
 	end
+	
+	-- hanadle the () operator for after-the-fact docs
 	function mt:__call(f)
 		docstrings[f] = {docs, unpack(arg)}
 		return f
 	end
 	return setmetatable({}, mt)
+end
+
+function help.addHelpExtension(func)
+	table.insert(helpExtensions, func)
 end
 
 
@@ -79,7 +73,6 @@ help(b)
 print("before defining random")
 random =
   help.docstring[[Compute random number.]] ..
-  decorator("number", '->', "number") ..
   function(n)
     return math.random(n)
   end
