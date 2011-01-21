4e077b9a404976c5be2b427fe2b0d42aa114c104
Ryan Pavlik <rpavlik@iastate.edu>
2011-01-21 15:48:40 -0600

    major actions cleanup+improvement + docs

--- vrjugglua/lua/Actions.lua
+++ vrjugglua/lua/Actions.lua
@@ -1,8 +1,15 @@
+require("help")
+
 Actions = {
 	_frameActions = {};
 }
 
-function Actions.addNodeAction(node, func)
+
+Actions.addNodeAction = help.docstring[[
+Given a node and a function, creates an action for the node to perform.
+By this, we mean: if the node is attached to the scenegraph, this Action
+will run.
+]] .. function(node, func)
 	local co = coroutine.create(func)
 	local c = osgLua.NodeCallback(
 		function(n, nodeVisitor)
@@ -16,13 +23,45 @@ function Actions.addNodeAction(node, func)
 	node:addUpdateCallback(c)
 end
 
-function Actions.addFrameAction(func)
+Actions.addFrameAction = help.docstring[[
+Given a function, start an action. Your function will be run, and
+you will be responsible for calling Actions.waitForRedraw() once you
+are done executing for a single display frame.
+
+Your action will be removed when it finishes (goes to the end or returns),
+or sooner if you use Actions.removeFrameAction.
+
+Returns an object referring to your action - only useful to remove it.
+If you don't plan on removing the action before it completes, you don't
+need to hang on to this value.
+]] .. function (func)
 	local co = coroutine.create(func)
 	table.insert(Actions._frameActions, co)
 	return co
 end
 
-function Actions.updateFrameActions()
+Actions.removeFrameAction = help.docstring[[
+Remove a frame action, stopping it from executing, presumably prior to
+its natural exit.
+]] .. function (action)
+	--- @todo not sure if this works
+	table.remove(Actions._frameActions, action)	
+end
+
+Actions.waitForRedraw = help.docstring[[
+Pauses an action momentarily to update the display. This must be called frequently:
+changes will not be seen until it is called.
+]] .. function ()
+	coroutine.yield()
+end
+
+Actions.updateFrameActions = help.docstring[[
+Run all frame actions until they wait for the next redraw. This
+should be placed preferably in the latePreFrame section of the application
+delegate.
+
+If that made no sense to you, you do not need to use this function.
+]] .. function ()
 	local thisTime = Actions._frameActions
 	Actions._frameActions = {}
 
@@ -34,4 +73,13 @@ function Actions.updateFrameActions()
 			table.insert(Actions._frameActions, v)
 		end
 	end
+end
+
+Actions.setAppProxy = help.docstring[[
+If you are implementing your own application delegate, call this method
+before adding any actions, and pass the value you got from vrjApp.OsgAppProxy().
+
+If this made no sense to you, you do not need to use this function.
+]] .. function(proxy)
+	Actions.appProxy = proxy
 end
