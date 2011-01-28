73d4dd67e25a243a8e9fb64b73b4ccf3519ec4f2
Ryan Pavlik <rpavlik@iastate.edu>
2011-01-28 14:44:35 -0600

    make the matrix test harder too

--- tests/luatests/osglua-matrixmath.lua
+++ tests/luatests/osglua-matrixmath.lua
@@ -1,14 +1,30 @@
 
 print("CTEST_FULL_OUTPUT")
+require("help")
 
-print(osg.Matrix.identity())
+function doSubtest(matrixType, vectorType)
+	assert((vectorType(5, 5, 5) * matrixType.identity()) == vectorType(5, 5, 5))
+	assert((vectorType(5, 5, 5) * matrixType.identity()) == vectorType(5, 5, 5))
+end
 
-print(osg.Matrix.identity() == osg.Matrix.identity())
-assert(osg.Matrix.identity() == osg.Matrix.identity())
+function doTest(matrixType)
+	print(" --- new test --- ")
+	print("Matrix type: " .. help.lookup(matrixType())["class"])
+	
+	print(matrixType.identity())
+	
+	print(matrixType.identity() == matrixType.identity())
+	assert(matrixType.identity() == matrixType.identity())
+	
+	assert((matrixType.identity() * matrixType.identity()) == matrixType.identity())
+	
+	doSubtest(matrixType, osg.Vec3)
+	doSubtest(matrixType, osg.Vec3f)
+	doSubtest(matrixType, osg.Vec3d)
+end
 
-assert((osg.Matrix.identity() * osg.Matrix.identity()) == osg.Matrix.identity())
-
-assert((osg.Vec3f(5, 5, 5) * osg.Matrix.identity()) == osg.Vec3f(5, 5, 5))
-assert((osg.Vec3(5, 5, 5) * osg.Matrix.identity()) == osg.Vec3(5, 5, 5))
+doTest(osg.Matrix)
+doTest(osg.Matrixd)
+doTest(osg.Matrixf)
 
 print("Done!")
