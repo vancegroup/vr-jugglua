280ad1afee6a74f2dd33c1e0a3d0914e7224e585
Ryan Pavlik <rpavlik@iastate.edu>
2011-01-31 13:07:07 -0600

    make sure there is an ISU copyright on all and only those files with modifications

--- third-party/osgLua/src/osgLua/Callback.cpp
+++ third-party/osgLua/src/osgLua/Callback.cpp
@@ -1,3 +1,18 @@
+/*
+	osgLua: use Lua to access dynamically to osg using osgIntrospection
+	Copyright(C) 2006 Jose L. Hidalgo Valiño (PpluX) (pplux at pplux.com)
+
+    This library is open source and may be redistributed and/or modified under
+    the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
+    (at your option) any later version.  The full license is in LICENSE file
+    included with this distribution, and on the openscenegraph.org website.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    OpenSceneGraph Public License for more details.
+*/
+
 #include <osgLua/Callback>
 #include <osgLua/Value>
 
--- third-party/osgLua/src/osgLua/osgLua.cpp
+++ third-party/osgLua/src/osgLua/osgLua.cpp
@@ -1,3 +1,19 @@
+/*
+	osgLua: use Lua to access dynamically to osg using osgIntrospection
+	Copyright(C) 2006 Jose L. Hidalgo Valiño (PpluX) (pplux at pplux.com)
+	Copyright(C) 2010-2011 Iowa State University (Author: Ryan Pavlik <rpavlik@acm.org> )
+
+    This library is open source and may be redistributed and/or modified under
+    the terms of the OpenSceneGraph Public License (OSGPL) version 0.0 or
+    (at your option) any later version.  The full license is in LICENSE file
+    included with this distribution, and on the openscenegraph.org website.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    OpenSceneGraph Public License for more details.
+*/
+
 #include <osgLua/LoadWrapper>
 #include "osgLua.h"
 
