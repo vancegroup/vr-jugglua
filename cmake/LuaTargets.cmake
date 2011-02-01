05d02fe689097420beadaaf37476c1311c01fad4
Ryan Pavlik <rpavlik@iastate.edu>
2011-02-01 10:14:06 -0600

    make lua targets actually try compiling

--- cmake/LuaTargets.cmake
+++ cmake/LuaTargets.cmake
@@ -27,8 +27,51 @@ set(__add_lua YES)
 
 include(FileCopyTargets)
 
-function(add_lua_target)
-	add_file_copy_target(${ARGN})
+function(add_lua_target _target _dest)
+	
+	if(NOT ARGN)
+		message(WARNING
+			"In add_lua_target call for target ${_target}, no source files were specified!")
+		return()
+	endif()
+	
+	if(NOT LUA_TARGET_LUAC_EXECUTABLE)
+		set(LUA_TARGET_LUAC_EXECUTABLE luac)
+	endif()
+
+	set(ALLFILES)
+	foreach(fn ${ARGN})
+		if(IS_ABSOLUTE "${fn}")
+			set(fullpath "${fn}")
+			get_filename_component(fn "${fn}" NAME)
+		else()
+			get_filename_component(fullpath "${CMAKE_CURRENT_SOURCE_DIR}/${fn}" ABSOLUTE)
+		endif()
+		add_custom_command(OUTPUT "${_dest}/${fn}"
+				COMMAND
+				${CMAKE_COMMAND}
+				ARGS -E make_directory "${_dest}"
+				COMMAND
+				${CMAKE_COMMAND}
+				ARGS -E copy "${fullpath}" "${_dest}"
+				COMMAND
+				"${LUA_TARGET_LUAC_EXECUTABLE}"
+				ARGS -p "${fullpath}"
+				WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+				DEPENDS "${fullpath}"
+				COMMENT "Copying ${fullpath} to ${_dest}/${fn} and parsing...")
+		list(APPEND ALLFILES "${_dest}/${fn}")
+	endforeach()
+
+	# Custom target depending on all the file copy commands
+	add_custom_target(${_target}
+		SOURCES ${ARGN}
+		DEPENDS ${ALLFILES})
+	if(TARGET "${LUA_TARGET_LUAC_EXECUTABLE}")
+		add_dependencies(${_target} ${LUA_TARGET_LUAC_EXECUTABLE})
+	endif()
+
+	set_property(TARGET ${_target} PROPERTY FILE_COPY_TARGET YES)
 endfunction()
 
 function(install_lua_target)
