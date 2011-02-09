bf362092213567e39dbdeeb459fa18984f123c53
Ryan Pavlik <rpavlik@iastate.edu>
2011-02-09 07:02:18 -0600

    add text example to minimal navtestbed example

--- examples/navtestbed-based/1-minimal.lua
+++ examples/navtestbed-based/1-minimal.lua
@@ -3,3 +3,14 @@ plane = Transform{
 	Model("examples/models/cessna.osg")
 }
 RelativeTo.World:addChild(plane)
+
+
+require("Text")
+
+RelativeTo.World:addChild(
+	TextGeode{
+		"First line",
+		"second line",
+		font = Font("DroidSans"),		
+	}
+)
