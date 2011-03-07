2352b635fe8fe029e917296b5d15661aabef853f
Ryan Pavlik <rpavlik@iastate.edu>
2011-03-07 11:10:14 -0600

    let depth be an optional param to getscriptfilename

--- vrjugglua/lua/getScriptFilename.lua
+++ vrjugglua/lua/getScriptFilename.lua
@@ -1,6 +1,6 @@
 
-function getScriptFilename()
-	local info = debug.getinfo(2, 'S')
+function getScriptFilename(depth)
+	local info = debug.getinfo(depth or 2, 'S')
 	-- split up the string
 	local prefix = string.sub(info.source, 1, 1)
 	if prefix == "@" then
