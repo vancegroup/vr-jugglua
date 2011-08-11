2227ccb5e89f21302da956c493f754ce0a737beb
Ryan Pavlik <rpavlik@iastate.edu>
2011-08-11 10:55:11 -0500

    handle disappearing cppcheck just fine

--- cmake/Findcppcheck.cmake
+++ cmake/Findcppcheck.cmake
@@ -39,6 +39,10 @@ set(CPPCHECK_ROOT_DIR
 set(_oldappbundlesetting ${CMAKE_FIND_APPBUNDLE})
 set(CMAKE_FIND_APPBUNDLE NEVER)
 
+if(CPPCHECK_EXECUTABLE AND NOT EXISTS "${CPPCHECK_EXECUTABLE}")
+	set(CPPCHECK_EXECUTABLE "notfound" CACHE PATH FORCE "")
+endif()
+
 # If we have a custom path, look there first.
 if(CPPCHECK_ROOT_DIR)
 	find_program(CPPCHECK_EXECUTABLE
