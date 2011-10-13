b998010da8fb614fe612c35dce0983f566809998
Ryan Pavlik <rpavlik@iastate.edu>
2011-10-13 17:47:11 -0500

    work on converting matrices

--- third-party/osgLua/src/osgLua/CustomConverters.cpp
+++ third-party/osgLua/src/osgLua/CustomConverters.cpp
@@ -23,6 +23,7 @@
 // Library/third-party includes
 #include <osgLua/introspection/Converter>
 #include <osgLua/introspection/ConverterProxy>
+#include <osgLua/introspection/variant_cast>
 
 #include <osg/Vec2d>
 #include <osg/Vec2f>
@@ -38,6 +39,9 @@
 #include <osg/Vec4b>
 #include <osg/Vec4ub>
 
+#include <osg/Matrixd>
+#include <osg/Matrixf>
+
 // Standard includes
 #include <iostream>
 
@@ -48,35 +52,85 @@ namespace osgLua {
 
 		template<typename SrcType, typename DestType>
 		class VectorPrecisionConverter : public introspection::Converter {
-			typedef typename DestType::value_type DestValueType;
-			enum { num_components = DestType::num_components };
-			typedef IntToType<DestType::num_components> NumComponentsType;
+				typedef typename DestType::value_type DestValueType;
+				enum { num_components = DestType::num_components };
+				typedef IntToType<DestType::num_components> NumComponentsType;
 
-			virtual ~VectorPrecisionConverter() {}
-			virtual introspection::Value convert(const introspection::Value& src) const
-			{
-				return convertVec(introspection::variant_cast<SrcType>(src), NumComponentsType());
+				virtual ~VectorPrecisionConverter() {}
+				virtual introspection::Value convert(const introspection::Value& src) const {
+					return convertVec(introspection::variant_cast<SrcType>(src), NumComponentsType());
+				}
+
+				DestType convertVec(SrcType const& input, IntToType<2>) const {
+					return DestType(static_cast<DestValueType>(input[0]),
+					                static_cast<DestValueType>(input[1]));
+				}
+
+				DestType convertVec(SrcType const& input, IntToType<3>) const {
+					return DestType(static_cast<DestValueType>(input[0]),
+					                static_cast<DestValueType>(input[1]),
+					                static_cast<DestValueType>(input[2]));
+				}
+
+				DestType convertVec(SrcType const& input, IntToType<4>) const {
+					return DestType(static_cast<DestValueType>(input[0]),
+					                static_cast<DestValueType>(input[1]),
+					                static_cast<DestValueType>(input[2]),
+					                static_cast<DestValueType>(input[3]));
+				}
+
+				virtual introspection::CastType getCastType() const {
+					return introspection::STATIC_CAST;
+				}
+		};
+
+
+		template<typename T>
+		struct MatrixInfo;
+
+		template<typename T>
+		struct ValMatrixInfo {
+			static typename T::value_type const * getPtr(const introspection::Value& src) {
+				return introspection::variant_cast<T>(src).ptr();
 			}
-
-			DestType convertVec(SrcType const& input, IntToType<2>) const {
-				return DestType(static_cast<DestValueType>(input[0]),
-					static_cast<DestValueType>(input[1]));
+			template<typename InputType>
+			static introspection::Value create(InputType input) {
+				return T(input);
 			}
+		};
 
-			DestType convertVec(SrcType const& input, IntToType<3>) const {
-				return DestType(static_cast<DestValueType>(input[0]),
-					static_cast<DestValueType>(input[1]),
-					static_cast<DestValueType>(input[2]));
+		template<typename T>
+		struct RefMatrixInfo {
+			static typename T::value_type const * getPtr(const introspection::Value& src) {
+				return introspection::variant_cast<T const *>(src)->ptr();
 			}
-
-			DestType convertVec(SrcType const& input, IntToType<4>) const {
-				return DestType(static_cast<DestValueType>(input[0]),
-					static_cast<DestValueType>(input[1]),
-					static_cast<DestValueType>(input[2]),
-					static_cast<DestValueType>(input[3]));
+			template<typename InputType>
+			static introspection::Value create(InputType input) {
+				return new T(input);
 			}
+		};
 
-			virtual introspection::CastType getCastType() const { return introspection::STATIC_CAST; }
+		template<>
+		struct MatrixInfo<osg::RefMatrixf> : RefMatrixInfo<osg::RefMatrixf> {};
+		template<>
+		struct MatrixInfo<osg::RefMatrixd> : RefMatrixInfo<osg::RefMatrixd> {};
+		template<>
+		struct MatrixInfo<osg::Matrixf> : ValMatrixInfo<osg::Matrixf> {};
+		template<>
+		struct MatrixInfo<osg::Matrixd> : ValMatrixInfo<osg::Matrixd> {};
+
+		template<typename SrcType, typename DestType>
+		class MatrixPrecisionConverter : public introspection::Converter {
+
+				virtual ~MatrixPrecisionConverter() {}
+				virtual introspection::Value convert(const introspection::Value& src) const {
+					return MatrixInfo<DestType>::create(MatrixInfo<SrcType>::getPtr(src));
+					//return MatrixInfo<DestType>::create(introspection::variant_cast<SrcType const&>(src).ptr());
+				}
+
+				virtual introspection::CastType getCastType() const {
+					return introspection::STATIC_CAST;
+				}
 		};
 
 		template<typename T>
@@ -84,10 +138,17 @@ namespace osgLua {
 			static introspection::Type const& t = typeof(T);
 			return t;
 		}
+
 		template<typename T1, typename T2>
 		inline void registerVectorPrecisionConverter() {
-			introspection::ConverterProxy cvt1to2(getIntrospectionType<T1>(), getIntrospectionType<T2>(), new VectorPrecisionConverter<T1, T2>());
-			introspection::ConverterProxy cvt2to1(getIntrospectionType<T2>(), getIntrospectionType<T1>(), new VectorPrecisionConverter<T2, T1>());
+			static introspection::ConverterProxy cvt1to2(getIntrospectionType<T1>(), getIntrospectionType<T2>(), new VectorPrecisionConverter<T1, T2>());
+			static introspection::ConverterProxy cvt2to1(getIntrospectionType<T2>(), getIntrospectionType<T1>(), new VectorPrecisionConverter<T2, T1>());
+		}
+
+		template<typename T1, typename T2>
+		inline void registerMatrixPrecisionConverter() {
+			static introspection::ConverterProxy cvt1to2(getIntrospectionType<T1>(), getIntrospectionType<T2>(), new MatrixPrecisionConverter<T1, T2>());
+			static introspection::ConverterProxy cvt2to1(getIntrospectionType<T2>(), getIntrospectionType<T1>(), new MatrixPrecisionConverter<T2, T1>());
 		}
 
 	} // end of anonymous namespace
@@ -103,5 +164,9 @@ namespace osgLua {
 		//registerVectorPrecisionConverter<osg::Vec4f, osg::Vec4s>();
 		registerVectorPrecisionConverter<osg::Vec4s, osg::Vec4b>();
 		registerVectorPrecisionConverter<osg::Vec4b, osg::Vec4ub>();
+
+		registerMatrixPrecisionConverter<osg::Matrixd, osg::Matrixf>();
+		registerMatrixPrecisionConverter<osg::Matrixd, osg::RefMatrixd>();
+		registerMatrixPrecisionConverter<osg::Matrixf, osg::RefMatrixf>();
 	}
 } // end of namespace osgLua
