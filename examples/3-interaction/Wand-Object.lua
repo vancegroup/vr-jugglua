8b395c268ada8033186fe896566b180b83e2a168
Patrick Carlson <carlsonp@iastate.edu>
2011-12-09 16:44:37 -0600

    add example where model can be attached to the wand

---
+++ examples/3-interaction/Wand-Object.lua
@@ -0,0 +1,23 @@
+require("Actions")
+
+--This example allows you to load in a model and have it
+--fixed to the wand.  This is usful in simulation mode for
+--loading in models and rotating and orienting them with
+--the keyboard and mouse for visualization.
+
+device = gadget.PositionInterface("VJWand")
+
+Actions.addFrameAction(function()
+	local xform = osg.MatrixTransform()
+	m = Model("examples/models/cessna.osg")
+	xform:addChild(m)
+
+	RelativeTo.Room:addChild(xform)
+
+	-- Update the cursor position forever.
+	while true do
+		xform:setMatrix(device.matrix)
+		Actions.waitForRedraw()
+	end
+end)
+
