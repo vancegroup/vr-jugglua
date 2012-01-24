223dfe81a4ab4e91ee3fcd88a6e3df65f646bba6
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-24 11:53:29 -0600

    broaden check for osgwrapper

--- doc/generation/is_osg_wrapper.lua
+++ doc/generation/is_osg_wrapper.lua
@@ -1,5 +1,5 @@
 return function(name)
-	if name == "OpenThreads" then return true end
-	if name == "osgLua" then return false end
+	if name:match("^OpenThreads") then return true end
+	if name:match("^osgLua") then return false end
 	return name:match("^osg") ~= nil
 end
