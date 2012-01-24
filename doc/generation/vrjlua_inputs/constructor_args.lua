2f9cd191cbc732a45e8399e91f2a78bf340eed04
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-24 12:40:20 -0600

    Add some constructor args

--- doc/generation/vrjlua_inputs/constructor_args.lua
+++ doc/generation/vrjlua_inputs/constructor_args.lua
@@ -1,3 +1,7 @@
 return {
+	["gadget.AnalogInterface"] = function() return "VJAnalog0" end,
+	["gadget.PositionInterface"] = function() return "VJHead" end,
 	["gadget.DigitalInterface"] = function() return "VJButton0" end,
+	--["snx.SoundInfo"] = function() return "SoundName" end,
+	["snx.SoundHandle"] = function() return "SoundName" end,
 }
