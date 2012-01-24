69c32de8ac69ab097b39a085c172bd657b56d9c1
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-24 10:41:43 -0600

    stash luabind globals

---
+++ doc/generation/luabind_globals.lua
@@ -0,0 +1,5 @@
+return {
+ ["class_info_data"] = true,
+ ["class_info"] = true,
+ ["class_names"] = true,
+}
--- doc/generation/vrjlua_docglobals.lua
+++ doc/generation/vrjlua_docglobals.lua
@@ -1,9 +1,10 @@
 #!/usr/bin/env lua
 
 local orig_globals = require "default_lua_globals"
+local luabind_globals = require "luabind_globals"
 
 for k, v in pairs(_G) do
-	if not orig_globals[k] then
-		print(k)
+	if not orig_globals[k] and not luabind_globals[k] then
+		print(k, type(v), v)
 	end
 end
