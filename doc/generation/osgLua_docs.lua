76cd83ba575c6cb6e9565f35cd3cc987665502e4
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-24 17:15:39 -0600

    tidy up osgLua generation

--- doc/generation/osgLua_docs.lua
+++ doc/generation/osgLua_docs.lua
@@ -4,17 +4,20 @@ local handled = {}
 
 local osgLuaDocs = setmetatable({
 	["getTypes"] = {
-		['description'] = [[Returns a table of all osgLua types, listed as their fully-qualified C++ name.]],
+		['description'] = [[
+Returns a table of all osgLua types, listed as their fully-qualified C++ name.]],
 		['params'] = {}
 	},
 	["saveObjectFile"] = {
-		['description'] = [[Pass an OSG object (node, etc) and a filename (likely ending in .osg).
-	Saves the given object to the named file]],
+		['description'] = [[
+Pass an OSG object (node, etc) and a filename (likely ending in .osg).
+Saves the given object to the named file]],
 		['params'] = {"object", "filename"}
 	},
 	["createByName"] = {
-		['description'] = [[Pass a string naming a type, and optionally parameters. Returns a new instance of the named type passing parameters to the constructor.
-	Usually can be ignored in favor of using the global tables directly to access constructors.]],
+		['description'] = [[
+Pass a string naming a type, and optionally parameters. Returns a new instance of the named type passing parameters to the constructor.
+Usually can be ignored in favor of using the global tables directly to access constructors.]],
 		['params'] = {"typename", "..."}
 	},
 	["loadObjectFile"] = {
@@ -26,13 +29,15 @@ local osgLuaDocs = setmetatable({
 		['params'] = {"callback"}
 	},
 	["getTypeInfo"] = {
-		['description'] = [[Pass a value or string. Returns a table with information about the given value's
-	type or the type specified by the given string.]],
+		['description'] = [[
+Pass a value or string. Returns a table with information about the given value's
+type or the type specified by the given string.]],
 		['params'] = {"valOrTypename"}
 	},
 	["loadWrapper"] = {
-		['description'] = [[Pass a string indicating an OSG nodekit to load (e.g. "osg", "osgText").
-	Loads the wrapper for that functionality, creating a corresponding global table providing access to it.]],
+		['description'] = [[
+Pass a string indicating an OSG nodekit to load (e.g. "osg", "osgText").
+Loads the wrapper for that functionality, creating a corresponding global table providing access to it.]],
 		['params'] = {"nodekit"}
 	},
 },
@@ -74,22 +79,30 @@ local outputMethod = function(methodName)
 	output()
 end
 
-output "---"
-outputCommentLine [[
+local outputSection = function(sectionName, desc)
+	output("--- " .. desc)
+	outputCommentLine("@section " .. sectionName)
+	output()
+end
+
+local outputModule = function(moduleName, desc)
+	output "---"
+	outputCommentLine(desc)
+	output(("module '%s'"):format(moduleName))
+	output()
+end
+
+outputModule("osgLua", [[
 This module uses the osgIntrospection library to provide nearly complete access to
 the C++ API of OpenSceneGraph.  In general, C++ OSG code can be converted with a few minor changes:
 
   * Namespace navigation and calls to static members are done using `.`, e.g. `osg.Group`
   * Member functions are called with `:`, e.g., `group:addChild(node)`
   * Some direct property access is available.
-]]
-output "module 'osgLua'"
-output()
+]])
 
 
-
-output "--- Explicit number type constructors"
-outputCommentLine [[@section numberconstructors]]
+outputSection("numberconstructors", "Explicit number type constructors")
 
 for methodName, _ in pairs(osgLua) do
 	if methodName:find("^GL") then
@@ -98,15 +111,13 @@ for methodName, _ in pairs(osgLua) do
 end
 
 
-output "--- Introspection functions"
-outputCommentLine [[@section introspect]]
+outputSection("introspect", "Introspection functions")
 
 for _, methodName in ipairs{"getTypeInfo", "getTypes"} do
 	outputMethod(methodName)
 end
 
-output "--- Other"
-outputCommentLine [[@section other]]
+outputSection("other", "Other")
 
 for methodName, _ in pairs(osgLua) do
 	outputMethod(methodName)
