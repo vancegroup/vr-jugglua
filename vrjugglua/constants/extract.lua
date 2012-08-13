f0b141cfd3e80bf94fdd12ad3747fa5a4dafae64
Ryan Pavlik <rpavlik@iastate.edu>
2012-08-13 17:18:43 -0500

    Add a lua script to extract GL defines in a header file to gldef.lua

---
+++ vrjugglua/constants/extract.lua
@@ -0,0 +1,21 @@
+
+tablename = arg[2] or "osgLua"
+
+local f = io.open(arg[1] or "gl.h", "rb")
+-- Grab everything between the " * Constants" and " * Miscellaneous" comment lines
+contents = f:read("*all"):gsub("^.- %* Constants(.-) %* Miscellaneous.*", "%1")
+f:close()
+
+-- Create table in case it doesn't exists
+print( ("%s = %s or {}"):format(tablename, tablename) )
+
+-- Populate table
+local defs = 0
+for defname, val in contents:gfind("#define (GL_[%w_]+)%s*(0x%x%x%x%x)") do
+	print( ("%s.%s = %s"):format(tablename, defname, val) )
+	defs = defs + 1
+end
+
+-- Friendly little comment.
+print("-- Total constants defined:", defs)
+
