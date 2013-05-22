b846d4f584e01d789a8a5eeb32d1c4a7f22f85b3
Ryan Pavlik <rpavlik@iastate.edu>
2013-05-22 12:33:51 -0500

    Don't recurse into osgBoost

--- third-party/osgLua/style-project.sh
+++ third-party/osgLua/style-project.sh
@@ -2,7 +2,7 @@
 
 PROJECT=$(cd $(dirname $0) && pwd)
 includes="Callback Config.in LoadWrapper LuaInclude Script Value"
-astyle -n --recursive "${PROJECT}/src/osgLua/*.cpp" --recursive "${PROJECT}/src/osgLua/*.h"
+astyle -n "${PROJECT}/src/osgLua/*.cpp" "${PROJECT}/src/osgLua/*.h"
 for fn in ${includes}; do
 	astyle -n "${PROJECT}/include/osgLua/${fn}"
 done
