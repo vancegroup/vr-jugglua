6bd460e75c6e88763f6d2300750f5aba7367b791
Ryan Pavlik <rpavlik@iastate.edu>
2013-10-25 16:55:29 -0500

    Add a little phong shading exmaple

---
+++ examples/advanced/phong-shading.lua
@@ -0,0 +1,48 @@
+-- Based on http://www.opengl.org/sdk/docs/tutorials/ClockworkCoders/lighting.php
+
+applyPhongToStateSet = function(stateset)
+    local vert = osg.Shader(osg.Shader.Type.VERTEX, [[
+        varying vec3 N;
+        varying vec3 v;
+        void main(void)  
+        {     
+           v = vec3(gl_ModelViewMatrix * gl_Vertex);       
+           N = normalize(gl_NormalMatrix * gl_Normal);
+           gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;  
+        }
+    ]])
+
+    local frag = osg.Shader(osg.Shader.Type.FRAGMENT, [[
+        varying vec3 N;
+        varying vec3 v;    
+        void main (void)  
+        {  
+           vec3 L = normalize(gl_LightSource[0].position.xyz - v);   
+           vec3 E = normalize(-v); // we are in Eye Coordinates, so EyePos is (0,0,0)  
+           vec3 R = normalize(-reflect(L,N));  
+         
+           //calculate Ambient Term:  
+           vec4 Iamb = gl_FrontLightProduct[0].ambient;    
+
+           //calculate Diffuse Term:  
+           vec4 Idiff = gl_FrontLightProduct[0].diffuse * max(dot(N,L), 0.0);
+           Idiff = clamp(Idiff, 0.0, 1.0);     
+           
+           // calculate Specular Term:
+           vec4 Ispec = gl_FrontLightProduct[0].specular 
+                        * pow(max(dot(R,E),0.0),0.3*gl_FrontMaterial.shininess);
+           Ispec = clamp(Ispec, 0.0, 1.0); 
+           // write Total Color:  
+           gl_FragColor = gl_FrontLightModelProduct.sceneColor + Iamb + Idiff + Ispec;     
+        }
+    ]])
+
+    local program = osg.Program()
+    program:addShader(vert)
+    program:addShader(frag)
+    stateset:setAttributeAndModes(program, osg.StateAttribute.Values.ON)
+end
+
+-- Run the following line to apply per-pixel lighting to the teapot from the LoadModel example.
+--applyPhongToStateSet(teapot:getOrCreateStateSet())
+
